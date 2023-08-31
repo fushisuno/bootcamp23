52a1e5df650c85ae7a67eb367cf0ce7f1537473c
fushisuno <114326061+fushisuno@users.noreply.github.com>
2023-08-31 13:56:08 +0000

    Testando editor de codigo do GitHub

---
+++ testeJv.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class testeJv{
+    
+    static Scanner ler = new Scanner(System.in);
+    public static void main(String[] args){
+        int sus = 0;
+        sus = ler.nextInt();
+        System.out.println("Valor de A é: " + sus);
+    }
+}
