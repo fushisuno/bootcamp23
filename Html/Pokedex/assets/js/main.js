528ea332938e103150961fdc71c9a7affb20235c
fushisuno <114326061+fushisuno@users.noreply.github.com>
2023-09-13 18:05:50 +0000

    #Title: Aprendendo a usar uma API
    #Changes: Utilizando a API de pokemons, adicionando os nomes,tipos, imagens, e id dos pokemons. Atualizando e melhorando a verbosidade dos codigos Js

--- Html/Pokedex/assets/js/main.js
+++ Html/Pokedex/assets/js/main.js
@@ -1,22 +1,24 @@
-
-function convertPokemonToLi(pokemon, i) {
+function convertPokemonToLi(pokemon) {
   return `
-    <li class="pokemon">
-        <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${
+          pokemon.id < 10 ? "00" : pokemon.id < 100 ? "0" : ""
+        }${pokemon.id}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
-                <li class="type">Grass</li>
-                <li class="type">Poison</li>
+            ${pokemon.types
+              .map((type) => `<li class="type ${type}">${type}</li>`)
+              .join("")}
             </ol>
-            <img src="https://seeklogo.com/images/B/bulbasaur-logo-8A27924C02-seeklogo.com.png" alt="${
-              pokemon.name
-            }">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>
     `;
 }
 
 pokeApi.getPokemons().then((pokemonList = []) => {
-    document.querySelector("#pokemons").innerHTML += pokemonList.map(convertPokemonToLi).join('')
+  document.querySelector("#pokemons").innerHTML += pokemonList
+    .map(convertPokemonToLi)
+    .join("");
 });
--- Html/Pokedex/assets/js/poke_api.js
+++ Html/Pokedex/assets/js/poke_api.js
@@ -1,32 +1,51 @@
 const pokeApi = {};
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+
+  pokemon.id = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+  return pokemon;
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
-    // Então pegamos um novo fetch da url do pokemon que quero acessar e converto a resposta num novo json dos detalhes
-  return fetch(pokemon.url).then((response) => response.json());
+  // Então pegamos um novo fetch da url do pokemon que quero acessar e converto a resposta num novo json dos detalhes
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
 };
 
-pokeApi.getPokemons = (ofset = 0, limit = 10) => {
+pokeApi.getPokemons = (ofset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?ofset=${ofset}&limit=${limit}`;
 
   // Retorna uma Promise - 'Promessa de uma resposta'
   // Processamento assincrono - não obtem resposta imediata
-  return fetch(url)
-    // Trás uma resposta que é transformada em json
-    .then((response) => response.json())
+  return (
+    fetch(url)
+      // Trás uma resposta que é transformada em json
+      .then((response) => response.json())
 
-    // Dentro do json, buscamos apenas o 'Body'/corpo dos resultados
-    .then((jsonBody) => jsonBody.results)
+      // Dentro do json, buscamos apenas o 'Body'/corpo dos resultados
+      .then((jsonBody) => jsonBody.results)
 
-    // Pegamos a lista de pokemons e mapeamos em uma lista de requisições do detalhe do pokemon
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+      // Pegamos a lista de pokemons e mapeamos em uma lista de requisições do detalhe do pokemon
+      .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
 
-    // Entçao esperamos que todas as requisições terminem
-    .then((requestDetails) => Promise.all(requestDetails))
-    .then((pokemonsDetails) => {
-      debugger;
-      console.log(pokemonsDetails);
-    })
-    .catch((error) => console.log(error));
+      // Então esperamos que todas as requisições terminem
+      .then((requestDetails) => Promise.all(requestDetails))
+
+      // Cria uma lista de detalhes dos pokemons
+      .then((pokemonsDetails) => pokemonsDetails)
+      .catch((error) => console.log(error))
+  );
 };
 
 //const op = {};
