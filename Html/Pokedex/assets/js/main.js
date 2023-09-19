24f96215d7285484259cdac5e4e3827b906c9d91
fushisuno <114326061+fushisuno@users.noreply.github.com>
2023-09-19 23:39:06 +0000

    #Title: Iniciando o desafio da pokedex
    #Changes: Iniciando o projeto proposto na DIO para a pokedex, criando o modelo html e csss

--- Html/Pokedex/assets/js/main.js
+++ Html/Pokedex/assets/js/main.js
@@ -5,20 +5,22 @@ let offset = 0;
 function loadPokemonItems(offset, limit){
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
     const newHtml = pokemons.map((pokemon)=>`
-      <li class="pokemon ${pokemon.type}">
-          <span class="number">#${
-            pokemon.id < 10 ? "00" : pokemon.id < 100 ? "0" : ""
-          }${pokemon.id}</span>
-          <span class="name">${pokemon.name}</span>
-          <div class="detail">
-              <ol class="types">
-              ${pokemon.types
-                .map((type) => `<li class="type ${type}">${type}</li>`)
-                .join("")}
-              </ol>
-              <img src="${pokemon.photo}" alt="${pokemon.name}">
-          </div>
-      </li>`).join("");
+    <a href="pokemon_detail.html">
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${
+              pokemon.id < 10 ? "00" : pokemon.id < 100 ? "0" : ""
+            }${pokemon.id}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                ${pokemon.types
+                  .map((type) => `<li class="type ${type}">${type}</li>`)
+                  .join("")}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>
+      </a>`).join("");
     document.querySelector("#pokemons").innerHTML += newHtml 
   });
 }
