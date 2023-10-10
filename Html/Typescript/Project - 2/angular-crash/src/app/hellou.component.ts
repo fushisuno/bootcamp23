9b273433d42faaca5c2d30cf384f6b71da637033
fushisuno <114326061+fushisuno@users.noreply.github.com>
2023-10-10 11:24:05 +0000

    #Title: Criando projetos com Typescript

---
+++ Html/Typescript/Project - 2/angular-crash/src/app/hellou.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-root',
+  template: '<h3> Sua mae é azul</h3>',
+  styles: ['']
+})
+export class AppComponent {
+  title = 'angular-crash';
+}
