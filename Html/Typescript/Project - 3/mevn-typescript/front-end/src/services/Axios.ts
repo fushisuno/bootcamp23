d42e6365e1c44806427f891ffb2a2607510fb579
Kainã Magdiel <kainamagdiel1920@gmail.com>
2023-10-16 22:28:33 +0000

    #Title: Error do WebPack

--- Html/Typescript/Project - 3/mevn-typescript/front-end/src/services/Axios.ts
+++ Html/Typescript/Project - 3/mevn-typescript/front-end/src/services/Axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "https://7000-fushisuno-bootcamp23-43vrt2rlr6y.ws-us105.gitpod.io/api",
+  baseURL: "https://7000-fushisuno-bootcamp23-43vrt2rlr6y.ws-us105.gitpod.io//api",
   headers: {
     'Content-Type': 'application/josn',
   },
